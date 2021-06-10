1298ec09017a4c7241f042e453bf98faba501b7f
Akhilraj Nambiar <nambiarakhilraj01@gmail.com>
2021-06-10 18:24:25 +0530

    Bubble sort visualizer

---
+++ bubbleSortVisual.py
@@ -0,0 +1,80 @@
+import pygame
+import random
+import time
+
+WIDTH = 1000
+HEIGHT = 600
+FPS = 30
+blue = (0, 0, 255)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+array = [random.randint(1,500) for i in range(32)]
+
+# Initializing pygame and setting game window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Bubble Sort Visualizer')
+clock = pygame.time.Clock()
+
+
+class Bars(pygame.sprite.Sprite):
+    def __init__(self, height, left):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((25, height))
+        self.image.fill(white)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = HEIGHT
+        self.rect.left = left
+        self.speed = 5
+
+def draw_text(surface, text, pos, size):
+    font_name = pygame.font.match_font('calibri')
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, red)
+    text_rect = text_surface.get_rect()
+    text_rect.topleft = pos
+    surface.blit(text_surface, text_rect)
+
+swoosh = pygame.mixer.Sound('swoosh.mp3')
+bub = pygame.mixer.Sound('bubble.mp3')
+running = True
+# Game loop
+while running:
+    clock.tick(FPS)
+    # event processing part
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Updating part
+    n = len(array)
+    for i in range(n):
+        for j in range(n - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                swoosh.play()
+                all_sprites = pygame.sprite.Group()
+                bars = []
+                first_left = 5
+                for i in array:
+                    bars.append(Bars(i, first_left))
+                    first_left += 30
+                key = bars[j]
+                key.image.fill(green)
+                next_to_key = bars[j+1]
+                next_to_key.image.fill(red)
+                for bar in bars:
+                    all_sprites.add(bar)
+                time.sleep(0.05)
+
+                all_sprites.update()
+                # rendering part
+                screen.fill(blue)
+                draw_text(screen, 'Bubble Sort Visualizer', (5, 0), 60)
+                all_sprites.draw(screen)
+                # after drawing everything we have to flip the screen
+                pygame.display.flip()
+
+pygame.quit()
---
+++ pygametemplate.py
@@ -0,0 +1,38 @@
+import pygame
+import random
+
+WIDTH = 500
+HEIGHT = 500
+FPS = 30
+blue = (0, 0, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+
+# Initializing pygame and setting game window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Bubble Sort Visualizer')
+clock = pygame.time.Clock()
+
+
+all_sprites = pygame.sprite.Group()
+running = True
+# Game loop
+while running:
+    clock.tick(FPS)
+    # event processing part
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Updating part
+    all_sprites.update()
+    # rendering part
+    screen.fill(blue)
+    all_sprites.draw(screen)
+    # after drawing everything we have to flip the screen
+    pygame.display.flip()
+
+pygame.quit()
